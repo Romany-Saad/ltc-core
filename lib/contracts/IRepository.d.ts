695b6e98c3ae3153876d4ec81cc07575e7494eed
romany-saad <romany1st@gmail.com>
2018-04-30 02:22:54 +0200

    add update method to IRepository interface

--- lib/contracts/IRepository.d.ts
+++ lib/contracts/IRepository.d.ts
@@ -1,8 +1,9 @@
 export default interface IRepository<T> {
-    findByIds(ids: string[]): Promise<T[]>;
-    find(query: object): Promise<T[]>;
     insert(items: T[]): Promise<T[]>;
-    remove(items: T[]): boolean;
+    find(query: object): Promise<T[]>;
+    findByIds(ids: string[]): Promise<T[]>;
+    update(items: T[]): Promise<boolean>;
+    remove(items: T[]): Promise<boolean>;
     parse(data: object): T;
     serialize(item: T): object;
 }
--- src/contracts/IRepository.ts
+++ src/contracts/IRepository.ts
@@ -1,9 +1,9 @@
 export default interface IRepository<T> {
 
   /*
-  * finds a set of items by ids
+  * inserts a set of items into collection
   * */
-  findByIds (ids: string[]): Promise<T[]>
+  insert (items: T[]): Promise<T[]>
 
   /*
   * query the collection to find a set of items
@@ -11,14 +11,19 @@ export default interface IRepository<T> {
   find (query: object): Promise<T[]>
 
   /*
-  * inserts a set of items into collection
+  * finds a set of items by ids
   * */
-  insert (items: T[]): Promise<T[]>
+  findByIds (ids: string[]): Promise<T[]>
+
+  /*
+  * updates a list of items in a collection
+  * */
+  update (items: T[]): Promise<boolean>
 
   /*
   * removes a set of items from collection
   * */
-  remove (items: T[]): boolean
+  remove (items: T[]): Promise<boolean>
 
   /*
   * parser a plain object and returns a model
