5d660a3089c6f502a791f24bb51bd5102df81952
romany-saad <romany1st@gmail.com>
2018-05-07 11:28:57 +0200

    fix a bug on isSerializable

--- __test__/utils.test.js
+++ __test__/utils.test.js
@@ -1,20 +1,32 @@
 const { isSerializable } = require("../lib/utils");
-describe("given an object with serialize method", () => {
+
+describe(`isSerializable is a function that checks 
+if a given object has serialize method or not`, () => {
+
   const x = {
     serialize() {
     },
   };
   const y = { serialize: 1 };
   const z = {};
-  it("utils.isSerializable should return true on objects that define serialize() method", () => {
+
+  it("should return true on objects that define serialize() method", () => {
     expect(isSerializable(x)).toBe(true);
   });
 
-  it("utils.isSerializable should return false on objects define serialize but not as function", () => {
+  it("should return false on objects define serialize but not as function", () => {
     expect(isSerializable(y)).toBe(false);
   });
 
-  it("utils.isSerializable should return false on objects doesn't define serialize()", () => {
+  it("should return false on objects doesn't define serialize()", () => {
     expect(isSerializable(z)).toBe(false);
+  });
+
+  it("should return false on scalar types", () => {
+    expect(isSerializable("test")).toBe(false);
+  });
+
+  it("should return false on null", () => {
+    expect(isSerializable(null)).toBe(false);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import { ISerializable } from "./contracts"
 
 export const isSerializable = (object: any): object is ISerializable => {
+  if (!object || typeof object !== "object") return false
   return 'serialize' in object && typeof object.serialize === "function"
 }
