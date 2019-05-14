22066f1e3d3b53d2032cd15ddf98f6793141d436
HabibBeshara <habib@cyber-crafts.com>
2019-05-14 16:07:38 +0200

    disabled x-powered-by

--- src/express.ts
+++ src/express.ts
@@ -1,8 +1,9 @@
 const Express = require('express')
-const cors = require("cors")
+const cors = require('cors')
 
 const express = Express()
 
 express.use(cors())
+express.disable('x-powered-by')
 
 export default express
