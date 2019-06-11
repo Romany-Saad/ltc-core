6537501c8f686f75c2a522e73393a49a0626750f
HabibBeshara <habib@cyber-crafts.com>
2019-06-11 08:31:53 +0200

    added memoizee

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,5 +3,7 @@ import * as contracts from './contracts';
 import * as utils from './utils/index';
 export { default as emitter } from './Emitter';
 export { names, contracts, utils };
+import memoize = require('memoizee');
+export { memoize };
 export * from './contracts';
 export default App;
